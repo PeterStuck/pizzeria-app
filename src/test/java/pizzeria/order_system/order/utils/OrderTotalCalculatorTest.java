cf2ab86f38bf485f14da24d0d459f1ec81afc9d6
Piotr Krawczyk <piotr.krawczyk@interia.pl>
2021-05-14 23:36:30 +0200

    add order total calculator

---
+++ src/test/java/pizzeria/order_system/order/utils/OrderTotalCalculatorTest.java
@@ -0,0 +1,56 @@
+package pizzeria.order_system.order.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pizzeria.order_system.menu.models.ItemCategory;
+import pizzeria.order_system.menu.models.MenuItem;
+import pizzeria.order_system.menu.models.Pizza;
+import pizzeria.order_system.order.models.Order;
+import pizzeria.order_system.order.models.OrderItem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class OrderTotalCalculatorTest {
+
+    private Order order;
+    private MenuItem menuItem;
+
+    @BeforeEach
+    void setUp() {
+        order = new Order();
+        menuItem = new Pizza(1, "TEST", 1F, ItemCategory.PIZZA, null);
+    }
+
+    @Test
+    void orderWithNoItemsShouldReturnZeroAsTotal() {
+        assertThat(OrderTotalCalculator.calculateOrderTotal(order), equalTo(0F));
+    }
+
+    @Test
+    void shouldReturnTotalOfOrderWithMoreItemsWithOneQuantity() {
+        MenuItem menuItem2 = new Pizza(2, "TEST", 1F, ItemCategory.PIZZA, null);
+        MenuItem menuItem3 = new Pizza(3, "TEST", 1F, ItemCategory.PIZZA, null);
+        order.addItemToOrder(menuItem);
+        order.addItemToOrder(menuItem2);
+        order.addItemToOrder(menuItem3);
+
+        assertThat(OrderTotalCalculator.calculateOrderTotal(order), equalTo(3F));
+    }
+
+    @Test
+    void shouldReturnValidTotalWithMoreItemsWithMoreQuantity() {
+        MenuItem menuItem2 = new Pizza(2, "TEST", 1F, ItemCategory.PIZZA, null);
+        MenuItem menuItem3 = new Pizza(3, "TEST", 1F, ItemCategory.PIZZA, null);
+        order.addItemToOrder(menuItem);
+        order.addItemToOrder(menuItem);
+        order.addItemToOrder(menuItem2);
+        order.addItemToOrder(menuItem2);
+        order.addItemToOrder(menuItem3);
+        order.addItemToOrder(menuItem3);
+
+        assertThat(OrderTotalCalculator.calculateOrderTotal(order), equalTo(6F));
+    }
+}
