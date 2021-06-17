61c3ceb921b6cacc1dc03e282e230f333bf33ee6
Piotr <piotr.krawczyk@interia.pl>
2021-06-17 18:47:22 +0200

    Remove old console ui

--- src/main/java/pizzeria/ui/printers/OrderPrinter.java
+++
@@ -1,9 +0,0 @@
-package pizzeria.ui.printers;
-
-import pizzeria.order_system.menu.exceptions.MenuItemNotFoundException;
-
-public interface OrderPrinter {
-
-    void showOrderSummary() throws MenuItemNotFoundException;
-
-}
